97b19ce02aa2f660afe401dc3e4f7c2dab689f9f
jaehyun <jtic0524@gmail.com>
2025-11-25 22:28:31 +0900

    feat: 병렬 처리

--- scripts/data.py
+++ scripts/data.py
@@ -14,6 +14,63 @@ from src.data.noise import BernoulliNoise
 from src.data.generator import DataGenerator
 from itertools import product
 import numpy as np
+from concurrent.futures import ProcessPoolExecutor, as_completed
+from tqdm import tqdm
+import os
+from typing import NamedTuple
+
+
+class ProcessArg(NamedTuple):
+    index: int
+    speed: int
+    direction: Direction
+    label: str
+    font_size: float
+    fps: int
+
+
+def process(arg: ProcessArg):
+    (
+        index,
+        speed,
+        direction,
+        label,
+        font_size,
+        fps,
+    ) = arg
+    n_position_sample = 25
+    binary = ("0", "1")
+    quad = ("A", "B", "C", "D")
+
+    directory = f"data/{index}"
+
+    np.random.seed(index)
+
+    transition = LinearTransition(direction=direction, total_frames=fps * 2, mpf=speed)
+
+    execute(
+        text_transition=transition,
+        text=label,
+        font_size_percent=font_size,
+        n_position_sample=n_position_sample,
+        fps=fps,
+        directory=directory,
+    )
+
+    return Metadata(
+        move_per_frame=speed,
+        move_direction=direction.name,
+        label=label,
+        options=binary if label in binary else quad,
+        fps=fps,
+        font_size=font_size,
+        length=2,
+        width=224,
+        height=224,
+        noise="BernoulliNoise(0.8)",
+        seed=index,
+        savedat=directory,
+    )
 
 
 def execute(
@@ -68,11 +125,9 @@ def main():
     speeds = (1, 3, 7)
     directions = (Direction.DOWN, Direction.UP_RIGHT)
     labels = tuple("01ABCD")
-    n_position_sample = 25
+    tasks = []
     font_sizes = (0.2, 0.4, 0.6)
     fpss = (10, 20, 30)
-    binary = ("0", "1")
-    quad = ("A", "B", "C", "D")
 
     for i, (
         speed,
@@ -89,34 +144,23 @@ def main():
             fpss,
         )
     ):
-        np.random.seed(i)
-        transition = LinearTransition(
-            direction=direction, total_frames=fps * 2, mpf=speed
-        )
+        tasks.append((i, speed, direction, label, font_size, fps))
 
-        execute(
-            text_transition=transition,
-            text=label,
-            font_size_percent=font_size,
-            n_position_sample=n_position_sample,
-            fps=fps,
-            directory=f"data/{i}",
-        )
+    metadata_path = "data/metadata.csv"
 
-        save_metadata(
-            path="data/metadata.csv",
-            metadata=Metadata(
-                move_per_frame=speed,
-                move_direction=direction.name,
-                label=label,
-                options=binary if label in binary else quad,
-                fps=fps,
-                font_size=font_size,
-                length=2,
-                width=224,
-                height=224,
-                noise="BernoulliNoise(0.8)",
-                seed=i,
-                savedat=f"data/{i}",
-            ),
-        )
+    if os.path.exists(metadata_path):
+        os.remove(metadata_path)
+
+    print(f"Total Tasks: {len(tasks)}")
+
+    with ProcessPoolExecutor() as executor:
+        futures = [executor.submit(process, task) for task in tasks]
+
+        for future in tqdm(as_completed(futures), total=len(tasks)):
+            metadata = future.result()
+
+            save_metadata(path=metadata_path, metadata=metadata)
+
+
+if __name__ == "__main__":
+    main()
