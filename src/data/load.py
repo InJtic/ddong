4707dccdd71c224aa1eb6bf56624f89d45dd34c1
jaehyun <jtic0524@gmail.com>
2025-11-26 19:21:54 +0900

    다운로드 및 불러오기 테스트

--- src/data/load.py
+++ src/data/load.py
@@ -1,29 +1,55 @@
 from torch.utils.data import Dataset
 import pandas as pd
-from decord import VideoReader, bridge, gpu
+from decord import VideoReader, bridge, cpu
 import os.path as osp
+from huggingface_hub import snapshot_download
+from typing import cast
+import torch
 
 bridge.set_bridge("torch")
 
 
 class VideoDataset(Dataset):
-    def __init__(self, metadata: str, root_dir: str = ""):
+    def __init__(self, repo_id: str = "Jtic/ddong-data", local_dir: str = "data/"):
+        metadata = osp.join(local_dir, "metadata.csv")
+
+        if osp.exists(local_dir):
+            # 이미 있는 경우
+            self.root_dir = local_dir
+
+        else:
+            self.root_dir = snapshot_download(
+                repo_id=repo_id,
+                repo_type="dataset",
+                local_dir=local_dir,
+            )
+
         self.metadata = pd.read_csv(metadata)
-        self.root_dir = root_dir
 
     def __len__(self):
         return len(self.metadata)
 
     def __getitem__(self, index: int):
         row = self.metadata.iloc[index]
-        video_path = [
-            osp.join(self.root_dir, row["savedat"], f"{i}.avi") for i in range(25)
-        ]
 
-        label = row["label"]
-        total_frames = row["fps"] * row["length"]
+        data_dir = osp.join(self.root_dir, row["savedat"])
 
-        vr = VideoReader(video_path, gpu())
-        video_tensor = vr.get_batch(list(range(total_frames))).permute(3, 0, 1, 2)
+        clips: list[torch.Tensor] = []
 
-        return video_tensor, label
+        for i in range(25):
+            data_path = osp.join(data_dir, f"{i}.avi")
+            vr = VideoReader(data_path, cpu())
+            clip = cast(torch.Tensor, vr.get_batch(range(len(vr)))).permute(3, 0, 1, 2)
+            clips.append(clip)
+
+        label: str = row["label"]
+
+        videoes = torch.cat(clips, dim=1)
+
+        return videoes, label
+
+
+if __name__ == "__main__":
+    dataset = VideoDataset()
+    videoes, label = dataset[0]
+    print(videoes.shape, label)
